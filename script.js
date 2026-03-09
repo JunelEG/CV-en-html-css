b0d5132e4acdf96d9cfb3ab65e7dd72840f88f11
JunelEG <juan.elizondo.garcia@est.una.ac.cr>
2026-03-09 12:26:43 -0600

    Refactor CSS for consistency and readability; update HTML structure and improve form handling in JavaScript

--- script.js
+++ script.js
@@ -1,28 +1,25 @@
-
 const themeBtn = document.getElementById("themeBtn");
 
 themeBtn.addEventListener("click", () => {
-    document.body.classList.toggle("light");
-}); 
+  document.body.classList.toggle("light");
+});
 
 const form = document.getElementById("contactForm");
 
-form.addEventListener("submit", function(e) {
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-    e.preventDefault(); 
+  const name = document.getElementById("name").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const message = document.getElementById("message").value.trim();
 
-    const name = document.getElementById("name").value.trim();
-    const email = document.getElementById("email").value.trim();
-    const message = document.getElementById("message").value.trim();
+  if (name === "" || email === "" || message === "") {
+    alert("Llene todos los campos");
+  } else {
+    alert("Mensaje enviado");
 
-    if (name === "" || email === "" || message === "") {
-        alert("Llene todos los campos");
-    } else {
-        alert("Mensaje enviado");
-
-        document.getElementById("name").value = "";
-        document.getElementById("email").value = "";
-        document.getElementById("message").value = "";
-    }
-
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+  }
 });
